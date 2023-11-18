c6d93b8762c18851b7246ff969fffdf0a371560a
Lazbert <85731679+Lazbert@users.noreply.github.com>
2023-11-19 04:21:20 +0800

    hide recommendation until flight selected

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import FlightTable from '@/components/FlightTable'
 import { FlightProps } from "@/components/FlightTable"
 import PilotScheduler from "./components/PilotScheduler"
 import PilotSelection from "./components/PilotSelection"
+import cx from "classnames"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -96,22 +97,30 @@ function App() {
             <FlightTable setFlight={setFlight} />
           </div>
           <div className="w-full flex flex-col">
-            <div className="h-full flex flex-col gap-2 p-4 rounded-lg bg-[#E5C6F3]">
-              <div className="flex justify-between">
-                <span className="text-[24px] font-medium">Recommendations</span>
-                <button className="mt-auto ml-auto px-4 py-2 rounded-[25px] bg-[#24BCDC] text-white">Confirm</button>
-              </div>
-              <div className="flex justify-center gap-2">
-                <div className="flex flex-col items-center gap-1">
-                  <span className="text-[20px] underline">Captain{ pilot1 && `: ${pilot1.displayName}` }</span>
-                  <PilotSelection recommendedPilots={recommendedPilot1} setSelectedPilot={setPilot1} />
+            { flight ? (
+                <div className="h-full flex flex-col gap-2 p-4 rounded-lg bg-[#E5C6F3]">
+                  <div className="flex justify-between">
+                    <span className="text-[24px] font-medium">Recommendations</span>
+                    <button className="mt-auto ml-auto px-4 py-2 rounded-[25px] bg-[#24BCDC] text-white">Confirm</button>
+                  </div>
+                  <div className={"flex justify-center gap-2"}>
+                    <div className="flex flex-col items-center gap-1">
+                      <span className="text-[20px] underline">Captain{ pilot1 && `: ${pilot1.displayName}` }</span>
+                      <PilotSelection recommendedPilots={recommendedPilot1} setSelectedPilot={setPilot1} />
+                    </div>
+                    <div className="flex flex-col items-center gap-1">
+                      <span className="text-[20px] underline">First Officer{ pilot2 && `: ${pilot2.displayName}` }</span>
+                      <PilotSelection recommendedPilots={recommendedPilot2} setSelectedPilot={setPilot2}/>
+                    </div>
+                  </div>
                 </div>
-                <div className="flex flex-col items-center gap-1">
-                  <span className="text-[20px] underline">First Officer{ pilot2 && `: ${pilot2.displayName}` }</span>
-                  <PilotSelection recommendedPilots={recommendedPilot2} setSelectedPilot={setPilot2}/>
+              ) : (
+                <div className="h-full flex justify-center items-center rounded-lg bg-transparent border border-dashed border-black/20">
+                  <span className="text-[24px] font-medium text-black/20">Select a flight on the left first</span>
                 </div>
-              </div>
-            </div>
+              )
+            
+            }
           </div>
         </div>
         <PilotScheduler />
